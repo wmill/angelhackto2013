1f5e994e84bdb713720d41554778b966d6683b28
Walter Miller <walter@waltermiller.ca>
2013-05-18 15:19:20 -0400

    Exchange rate example working

--- oanda_samples.py
+++ oanda_samples.py
@@ -6,5 +6,9 @@ def get_exchange_rate(instrument):
 	ie, USD_EUR, BTC_USD, XAU_USD, WHEAT_USD
 	"""
 	response = requests.get("http://api-sandbox.oanda.com/v1/instruments/%s/price" % instrument)
-	return response.json
-	
+	return response.json()
+
+if __name__ == '__main__':
+	rate = get_exchange_rate('XAU_USD')
+	#gold, gold!
+	print "Current price of Gold is USD:", rate['ask']
