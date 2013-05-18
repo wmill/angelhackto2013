bdc757dabe376f0446a59049e41221faa8d653f8
Walter Miller <walter@waltermiller.ca>
2013-05-18 15:28:04 -0400

    Another quick example

--- oanda_samples.py
+++ oanda_samples.py
@@ -12,3 +12,12 @@ if __name__ == '__main__':
 	rate = get_exchange_rate('XAU_USD')
 	#gold, gold!
 	print "Current price of Gold is USD:", rate['ask']
+
+	#spin wheat into gold!
+
+	gold_rate = get_exchange_rate('XAU_USD')
+	wheat_rate = get_exchange_rate('WHEAT_USD')
+
+	wheat_per_gold = gold_rate['ask'] / wheat_rate['ask']
+
+	print "You can buy an ouce of gold for %s bushels of wheat" % wheat_per_gold
