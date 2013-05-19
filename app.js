7f6f393b83794f5f71b58a1ff2a361bb21b8f6f5
Walter Miller <walter@waltermiller.ca>
2013-05-18 21:55:11 -0400

    Added handlebars for templates

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
-var chat;
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+
+var hbs = require('hbs');
 
 var tvs = {};
 
@@ -12,9 +16,10 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-app.get('/bacon', function (req, res){
-	chat.emit("bacon", {'tasty' : true});
-	res.sendfile(__dirname + '/index.html');
+
+app.get('/tv/', function (req, res){
+	//input the tv code
+	res.render('enter_tv_id');
 });
 
 app.get('/tv/:tv_id', function (req, res){
@@ -24,23 +29,24 @@ app.get('/tv/:tv_id', function (req, res){
 	if (!(tv_id in tvs)) {
 		tvs[tv_id] = io.of('/tv/' + tv_id);
 	}
+	//need to pass params to template, 
+	res.locals = {'tv_id': tv_id};
+	res.render('tv');
 });
 
-chat = io
-  .of('/chat')
-  .on('connection', function (socket) {
-    socket.emit('a message', {
-        that: 'only'
-      , '/chat': 'will get'
-    });
-    chat.emit('a message', {
-        everyone: 'in'
-      , '/chat': 'will get'
-    });
-  });
+app.get('/tv/:tv_id/item/:item_id', function (req, res){
+	//req.params.tv_id
+	var tv_id = req.params.tv_id;
+	//create the socket if it doesn't exist
+	if (!(tv_id in tvs)) {
+		console.log('bad tv_id: ', tv_id)
+		res.render('enter_tv_id');
+		return;
+	}
+	//fetch item info some how
+	var item_details  = {"stuff":""}
+	tvs[item_id].emit('show_item', item_details)
+	//need to pass params to template, 
+	res.render('item_sent')
+});
 
-var news = io
-  .of('/news')
-  .on('connection', function (socket) {
-    socket.emit('item', { news: 'item' });
-  });
