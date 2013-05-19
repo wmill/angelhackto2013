9593501a0fe759118c7a47e47ca545d140ac7e0b
Walter Miller <walter@waltermiller.ca>
2013-05-19 00:56:38 -0400

    Added mp4 support

--- public/tv.js
+++ public/tv.js
@@ -6,16 +6,19 @@ setup_sockets = function(tv_id){
     var url = item.url;
     var type = item.type; 
     var new_content;
-    if (type === 'video')
-    {
+    if (type === 'video'){
       new_content = $('<iframe />', {
         name: 'myframe',
-        src: url    
+        src: url
       });
     }
-    else
-    {
+    else if (type === 'picture') {
       new_content = $('<img class="img" src=' + url + '></img>');
+    }
+    else if (type === 'mp4') {
+      new_content = $('<video autoplay controls><source src="'
+        + url + '" type="video/mp4"></video>');
+      console.log(new_content);
     }
     $('#content').html(new_content);
     console.log(item);
