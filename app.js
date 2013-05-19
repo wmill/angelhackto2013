aa2a352e1d75310206b9740f976a2bbce2d0b408
Walter Miller <walter@waltermiller.ca>
2013-05-18 20:19:28 -0400

    Playing with sample code

--- app.js
+++ app.js
@@ -2,15 +2,35 @@ var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
+var chat;
+
 server.listen(8000);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+app.get('/bacon', function (req, res){
+	chat.emit("bacon", {'tasty' : true});
+	res.sendfile(__dirname + '/index.html');
 });
+
+
+chat = io
+  .of('/chat')
+  .on('connection', function (socket) {
+    socket.emit('a message', {
+        that: 'only'
+      , '/chat': 'will get'
+    });
+    chat.emit('a message', {
+        everyone: 'in'
+      , '/chat': 'will get'
+    });
+  });
+
+var news = io
+  .of('/news')
+  .on('connection', function (socket) {
+    socket.emit('item', { news: 'item' });
+  });
